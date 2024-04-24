36bbe2f2b41abcf5d4d7d2fca43b40ac36518236
AhmadrezaHosseiny <a.r.hosseiny1384@gmail.com>
2024-04-24 19:31:58 +0330

    done

--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -3,7 +3,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
